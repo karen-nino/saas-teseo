7423ccd9702abd87954ce245a613f7acc4197ae2
karen-nino <46912342+karen-nino@users.noreply.github.com>
2023-07-24 14:27:47 -0600

    fonts

--- saas-teseo/tailwind.config.js
+++ saas-teseo/tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        'plus-jakarta': ['"Plus Jakarta Sans"', 'sans-serif'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
